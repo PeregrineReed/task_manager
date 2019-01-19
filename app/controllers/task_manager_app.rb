96ab5f17f231e81372170f6b656a584addb98309
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-01-18 17:29:38 -0700

    Moved method override back to original position

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -2,6 +2,7 @@ require_relative '../models/task.rb'
 
 class TaskManagerApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
+  set :method_override, true
 
   get '/' do
     erb :dashboard
@@ -31,8 +32,6 @@ class TaskManagerApp < Sinatra::Base
     @task = Task.find(params[:id])
     erb :edit
   end
-  
-  set :method_override, true # this allows us to use _method in the form
 
   put '/tasks/:id' do |id|
     Task.update(id.to_i, params[:task])
